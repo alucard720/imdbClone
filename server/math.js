96e023fd964e436c5245c8a19536a00f00cb9a82
alucard720 <ing.santana36@gmail.com>
2023-05-17 15:15:03 -0400

    mac1.0.1

--- server/math.js
+++
@@ -1,7 +0,0 @@
-const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
-const multiply =(a, b) => a * b;
-const divide =(a, b) => a / b;
-
-
-module.exports = {add, substract, multiply, divide}
